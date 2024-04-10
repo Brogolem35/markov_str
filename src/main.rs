0fba257e7bf872a234a8bf451b0bbb84f6e74f6a
Brogolem35 <brogolem35@protonmail.com>
2024-04-10 20:19:24 +0300

    Get all files pointed by the env var

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,19 @@ use std::{env, fs};
 fn main() {
     let home_dir = env::var("HOME").expect("HOME Environment Variable not found");
 
+    let paths = fs::read_dir(&home_dir).expect(&format!("Can't read files from: {}", home_dir));
+
+    // Only the files remain
+    let files = paths
+        .filter_map(|f| f.ok())
+        .filter(|f| match f.file_type() {
+            Err(_) => false,
+            Ok(f) => f.is_file(),
+        });
+
+    for file in files {
+        println!("Name: {}", file.path().display())
+    }
+
     println!("{}", home_dir);
 }
