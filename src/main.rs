3544036c5952d6573bcdfc473f1b523a3da342a6
Brogolem35 <brogolem35@protonmail.com>
2024-04-10 22:11:55 +0300

    Add rustfmt and reformat

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,32 @@
 use std::{
-    collections::HashMap,
-    env,
-    fs::{self, read_to_string},
+	collections::HashMap,
+	env,
+	fs::{self, read_to_string},
 };
 
 struct ChainItem {
-    items: Vec<(String, u32)>,
-    totalcnt: u32,
+	items: Vec<(String, u32)>,
+	totalcnt: u32,
 }
 
 fn main() {
-    let home_dir = env::var("HOME").expect("HOME Environment Variable not found");
+	let home_dir = env::var("HOME").expect("HOME Environment Variable not found");
 
-    let paths = fs::read_dir(&home_dir).expect(&format!("Can't read files from: {}", home_dir));
+	let paths = fs::read_dir(&home_dir).expect(&format!("Can't read files from: {}", home_dir));
 
-    // Only the files remain
-    let files = paths
-        .filter_map(|f| f.ok())
-        .filter(|f| match f.file_type() {
-            Err(_) => false,
-            Ok(f) => f.is_file(),
-        });
+	// Only the files remain
+	let files = paths
+		.filter_map(|f| f.ok())
+		.filter(|f| match f.file_type() {
+			Err(_) => false,
+			Ok(f) => f.is_file(),
+		});
 
-    let strings = files.filter_map(|f| read_to_string(f.path()).ok());
+	let strings = files.filter_map(|f| read_to_string(f.path()).ok());
 
-    for s in strings {
-        println!("{}", s);
-    }
+	for s in strings {
+		println!("{}", s);
+	}
 
-    println!("{}", home_dir);
+	println!("{}", home_dir);
 }
