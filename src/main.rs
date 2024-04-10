7ad1df85776ed23d361b0de9e46a8292b8182d48
Brogolem35 <brogolem35@protonmail.com>
2024-04-10 20:07:02 +0300

    Read env var

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let home_dir = env::var("HOME").expect("HOME Environment Variable not found");
+
+    println!("{}", home_dir);
 }
