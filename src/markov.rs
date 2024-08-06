afaa93244e3b09f51b131fc486ba7c30e1d66688
Brogolem35 <brogolem35@protonmail.com>
2024-08-06 22:03:47 +0300

    Update tests

--- src/markov.rs
+++ src/markov.rs
@@ -181,17 +181,52 @@ mod tests {
 	use super::*;
 
 	#[test]
-	fn parser() {
-		let word_regex = Regex::new(r"(\w|\d|'|-)+(\.|!|\?)*").unwrap();
+	fn regex1() {
+		let rres: Vec<_> = WORD_REGEX
+			.find_iter("lorem ipsum dolor")
+			.map(|m| m.as_str())
+			.collect();
 
-		let text = "asfd asa... lmao.lorem swag!!?? kek     	p bla,bla ğ!.?ü";
+		assert_eq!(rres, vec!["lorem", "ipsum", "dolor"]);
+	}
 
-		assert_eq!(
-			find_words(text),
-			word_regex
-				.find_iter(text)
-				.map(|m| m.as_str().to_string())
-				.collect::<Vec<String>>()
-		);
+	#[test]
+	fn regex2() {
+		let rres: Vec<_> = WORD_REGEX
+			.find_iter("lor.em ips!um 'dolor")
+			.map(|m| m.as_str())
+			.collect();
+
+		assert_eq!(rres, vec!["lor", "em", "ips", "um", "dolor"]);
+	}
+
+	#[test]
+	fn regex3() {
+		let rres: Vec<_> = WORD_REGEX
+			.find_iter("lorem ipsum dol_3or")
+			.map(|m| m.as_str())
+			.collect();
+
+		assert_eq!(rres, vec!["lorem", "ipsum", "dol", "3or"]);
+	}
+
+	#[test]
+	fn regex4() {
+		let rres: Vec<_> = WORD_REGEX
+			.find_iter("123  1,23 1_2 2d3")
+			.map(|m| m.as_str())
+			.collect();
+
+		assert_eq!(rres, vec!["123", "1", "23", "1", "2", "2d3"]);
+	}
+
+	#[test]
+	fn regex5() {
+		let rres: Vec<_> = WORD_REGEX
+			.find_iter("ömür ğğğ 式 2d3")
+			.map(|m| m.as_str())
+			.collect();
+
+		assert_eq!(rres, vec!["ömür", "ğğğ", "式", "2d3"]);
 	}
 }
